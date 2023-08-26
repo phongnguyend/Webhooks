6299df1b1d2e76ab99cd2079554be5f4be775db3
Phong Nguyen <nguyendoanphong.it@gmail.com>
2023-08-26 09:35:04 +0700

    Code Init

---
+++ src/WebhookServer/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var requests = new List<RecivedRequest>();
+
+app.MapPost("/test", async (HttpRequest request) =>
+{
+    using var reader = new StreamReader(request.Body);
+    string text = await reader.ReadToEndAsync();
+
+    requests.Add(new RecivedRequest { Payload = text, RecievedDate = DateTimeOffset.Now });
+
+    return Results.Text(text, "application/json");
+});
+
+app.MapGet("/", () => requests);
+
+app.Run();
+
+class RecivedRequest
+{
+    public DateTimeOffset RecievedDate { get; set; }
+
+    public string? Payload { get; set; }
+}
